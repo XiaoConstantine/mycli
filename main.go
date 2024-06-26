4cbdea2929233a723c8c04b5bcc58581db016efa
Xiao Cui <constantine124@gmail.com>
2024-06-26 12:47:11 -0400

    Update per gh structure

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "mycli/cmd"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"mycli/cmd"
+)
+
+type exitCode int
+
+const (
+	exitOK      exitCode = 0
+	exitError   exitCode = 1
+	exitCancel  exitCode = 2
+	exitAuth    exitCode = 4
+	exitPending exitCode = 8
+)
 
 func main() {
-	cmd.Execute()
+	code := mainRun()
+	os.Exit(int(code))
+}
+
+func mainRun() exitCode {
+	rootCmd, err := cmd.NewRootCmd()
+	// stderr := cmdFactory.IOStreams.ErrOut
+	ctx := context.Background()
+
+	if err != nil {
+		fmt.Println("failed to create root command: %s\n", err)
+		return exitError
+	}
+	if command, err := rootCmd.ExecuteContextC(ctx); err != nil {
+		// printError(stderr, err, cmd, hasDebug)
+		fmt.Println("%s", command)
+
+	}
+	return exitOK
+
 }
