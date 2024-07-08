a1df10ba2e14537f6e4b9a0a6a8ba3c429c20ebb
Xiao <constantine124@gmail.com>
2024-07-08 12:40:52 -0400

    Increase coverage for color (#21)
    * Increase coverage for color
    
    * Add unit test for main

--- pkg/iostreams/color_test.go
+++ pkg/iostreams/color_test.go
@@ -93,3 +93,66 @@ func TestHexToRGB(t *testing.T) {
 		assert.Equal(t, tt.wants, output)
 	}
 }
+
+func TestHexToRGBInvalidHex(t *testing.T) {
+	cs := NewColorScheme(true, true, true)
+	output := cs.HexToRGB("XYZ", "text")
+	assert.Equal(t, "text", output)
+}
+
+func TestCScheme(t *testing.T) {
+	cs := NewColorScheme(true, true, true)
+	assert.True(t, cs.Enabled())
+
+	csDisabled := NewColorScheme(false, false, false)
+	assert.False(t, csDisabled.Enabled())
+
+	testCases := []struct {
+		name     string
+		input    string
+		expected string
+		fn       func(string) string
+	}{
+		{"Bold", "text", "\x1b[0;1;39mtext\x1b[0m", cs.Bold},
+		{"Red", "text", "\x1b[0;31mtext\x1b[0m", cs.Red},
+		{"Yellow", "text", "\x1b[0;33mtext\x1b[0m", cs.Yellow},
+		{"Green", "text", "\x1b[0;32mtext\x1b[0m", cs.Green},
+		{"GreenBold", "text", "\x1b[0;1;32mtext\x1b[0m", cs.GreenBold},
+		{"Gray", "text", "\x1b[38;5;242mtext\x1b[m", cs.Gray},
+		{"LightGrayUnderline", "text", "\x1b[0;2;4;37mtext\x1b[0m", cs.LightGrayUnderline},
+		{"Magenta", "text", "\x1b[0;35mtext\x1b[0m", cs.Magenta},
+		{"Cyan", "text", "\x1b[0;36mtext\x1b[0m", cs.Cyan},
+		{"CyanBold", "text", "\x1b[0;1;36mtext\x1b[0m", cs.CyanBold},
+		{"Blue", "text", "\x1b[0;34mtext\x1b[0m", cs.Blue},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, tc.fn(tc.input))
+			assert.Equal(t, tc.input, csDisabled.ColorFromString(tc.name)(tc.input))
+		})
+	}
+
+	// Test format functions
+	assert.Equal(t, "\x1b[0;1;39mHello, World!\x1b[0m", cs.Boldf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;31mHello, World!\x1b[0m", cs.Redf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;33mHello, World!\x1b[0m", cs.Yellowf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;32mHello, World!\x1b[0m", cs.Greenf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[38;5;242mHello, World!\x1b[m", cs.Grayf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;35mHello, World!\x1b[0m", cs.Magentaf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;36mHello, World!\x1b[0m", cs.Cyanf("Hello, %s!", "World"))
+	assert.Equal(t, "\x1b[0;34mHello, World!\x1b[0m", cs.Bluef("Hello, %s!", "World"))
+
+	// Test icons
+	assert.Equal(t, "\x1b[0;32m✓\x1b[0m", cs.SuccessIcon())
+	assert.Equal(t, "\x1b[0;33m!\x1b[0m", cs.WarningIcon())
+	assert.Equal(t, "\x1b[0;31mX\x1b[0m", cs.FailureIcon())
+
+	// Test ColorFromString
+	assert.Equal(t, "\x1b[0;1;39mtext\x1b[0m", cs.ColorFromString("bold")("text"))
+	assert.Equal(t, "text", cs.ColorFromString("invalid")("text"))
+
+	// Test SuccessIconWithColor and FailureIconWithColor
+	assert.Equal(t, "\x1b[0;34m✓\x1b[0m", cs.SuccessIconWithColor(cs.Blue))
+	assert.Equal(t, "\x1b[0;34mX\x1b[0m", cs.FailureIconWithColor(cs.Blue))
+}
